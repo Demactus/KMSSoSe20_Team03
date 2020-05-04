b9725f592da1a8cdaa00c46da6dd80846c4e3f96
Andreas Wagner <andreas.wagner@mni.thm.de>
2020-05-04 12:28:57 +0200

    added modell added create function for tasks

--- client/client.ts
+++ client/client.ts
@@ -1,3 +1,25 @@
-require('bootstrap');
+class Task {
+     id : number;
+     name: string;
+     date : Date;
+    description : string;
 
-console.log("todo");
+    constructor(id: number, name: string, description: string) {
+        this.id= id;
+        this.name = name;
+        this.date = new Date();
+        this.description = description;
+    }
+}
+
+//import { Task } from "./modell/task";
+
+let taskList : Task[] = [];
+
+function createAufgabe(id: number, name: string, description: string) {
+    taskList.push(new Task(id,name,description))
+}
+console.log("leere liste: " + taskList);
+
+createAufgabe(1,"test","ich bin ein test");
+console.log("keine leere liste: " + taskList[0].id + ", " + taskList[0].name + ", " +taskList[0].description);
---
+++ client/modell/task.ts
@@ -0,0 +1,14 @@
+/** class Task {
+    private id : number;
+    private name: string;
+    private date : Date;
+    private description : string;
+
+     constructor(id: number, name: string, description: string) {
+         this.id= id;
+         this.name = name;
+         this.date = new Date();
+         this.description = description;
+     }
+}
+*/
