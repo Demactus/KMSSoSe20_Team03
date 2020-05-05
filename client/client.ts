4fabf294a590d49cac2bc1723d2a53741f0af54d
Andreas Wagner <andreas.wagner@mni.thm.de>
2020-05-05 20:52:33 +0200

    fixed popper.js error added Category Class added Jquery support some other stuff

--- client/client.ts
+++ client/client.ts
@@ -1,25 +1,109 @@
+/**********************************************************************************************************************
+ * SECTION TASK
+ * FUNCTIONS GO HERE
+ *********************************************************************************************************************/
+
 class Task {
      id : number;
      name: string;
      date : Date;
-    description : string;
+     description : string;
+     category: Category;
 
-    constructor(id: number, name: string, description: string) {
+    constructor(id: number, name: string, description: string, category?: Category) {
         this.id= id;
         this.name = name;
         this.date = new Date();
         this.description = description;
+        this.category = category;
     }
 }
 
 //import { Task } from "./modell/task";
 
 let taskList : Task[] = [];
+let categoryList : Category[] = [];
 
-function createAufgabe(id: number, name: string, description: string) {
-    taskList.push(new Task(id,name,description))
+function calcCurrentTaskId() {
+    let id: number;
+    if (taskList.length <= 0) {
+        id = 0;
+    }else {
+        id = taskList.length;
+    }
+   return id++
+}
+function calcCurrentCatId() {
+    let id: number;
+    if (categoryList.length <= 0) {
+        id = 0;
+    }else {
+        id = categoryList.length;
+    }
+   return id++
+}
+
+function createTask( name: string, description: string) {
+    taskList.push(new Task(calcCurrentTaskId(), name, description))
 }
 console.log("leere liste: " + taskList);
 
-createAufgabe(1,"test","ich bin ein test");
-console.log("keine leere liste: " + taskList[0].id + ", " + taskList[0].name + ", " +taskList[0].description);
+createTask("test","ich bin ein test");
+createTask("test1","ich bin ein test1");
+createTask("test2","ich bin ein test2");
+createTask("test3","ich bin ein test3");
+
+/**
+ * Print all Tasks
+ */
+for ( let item of taskList){
+    console.log("keine leere liste: " + item.id + ", " + item.name + ", " + item.description);
+}
+
+
+
+
+/**********************************************************************************************************************
+ * SECTION CATEGORY
+ * FUNCTIONS GO HERE
+ *********************************************************************************************************************/
+
+class Category {
+    id : number;
+    name: string;
+    color: string;
+
+    constructor(id: number, name: string, color?: string){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+}
+
+function createCategory(name: string, color: string){
+    categoryList.push(new Category(calcCurrentCatId(), name, color))
+}
+
+console.log("leere liste: " + categoryList);
+
+createCategory("testA","ich bin ein test");
+createCategory("testB","ich bin ein test1");
+createCategory("testC","ich bin ein test2");
+createCategory("testD","ich bin ein test3");
+
+/**
+ * Print all Categories
+ */
+for ( let item of categoryList){
+    console.log("keine leere liste: " + item.id + ", " + item.name + ", " + item.color);
+}
+
+
+/**********************************************************************************************************************
+ * JQUERY MAIN EVENT LISTENER
+ *********************************************************************************************************************/
+$(function(){
+    console.log("JQuery working...")
+    // jQuery methods go here....
+  
+  });
